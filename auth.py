066d1cf7adf88b437f77154ed0ea83de3cbeee28
Lewis <lewiskerich11@gmail.com>
2026-07-13 20:13:57 +0300

    Delete auth.py

--- auth.py
+++
@@ -1,7 +0,0 @@
-#program tht captures username and password then compare with predefined values. if the values match , programm display , authentication successful , else program displays authentication failure
-password= input('Enter your password\n')
-username= input('Enter your username\n')
-if username== 'joel' and password=='12345':
-    print('Authentication successful!!!\n')
-else:
-    print('Authentication failure!!!\n')    
